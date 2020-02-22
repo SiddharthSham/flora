b2bc5b8610a2c017046fdbc2f979f9f13a9fb237
siddharthsham <siddhsham@gmail.com>
2020-02-22 14:55:52 +0530

    overhaul chrys and violet

--- Violet/src/db.js
+++ Violet/src/db.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 import 'firebase/firebase-firestore';
 
 const firebaseConfig = {
-    //your config here
+    apiKey: "AIzaSyCiFHdR0PMJ6O1Cdh5pQc2zse1QGC8k3X8",
+    authDomain: "violet-5f474.firebaseapp.com",
+    databaseURL: "https://violet-5f474.firebaseio.com",
+    projectId: "violet-5f474",
+    storageBucket: "violet-5f474.appspot.com",
+    messagingSenderId: "1043517061196",
+    appId: "1:1043517061196:web:f1f222aa60a4363cb25ce9",
+    measurementId: "G-RCP5XPVV7S"
 };
 
 firebase.initializeApp(firebaseConfig);
